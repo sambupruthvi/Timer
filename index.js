bb26450790fc6ee28d3c0b2ca74fa3ca9e8f946c
Pruthvi <Sambupruthvi@gmail.com>
2020-05-21 15:49:27 -0500

    adding callbacks and refactoring the code

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-class Timer{
-    constructor(durationInput, playButton, pauseButton){
-        this.durationInput = durationInput;
-        this.playButton = playButton;
-        this.pauseButton = pauseButton;
-
-        this.playButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-    }
-
-    //the reason we are using a arrow functions is because we can use 'this' keyword to access the methods and properties of that particular instance of the object
-    start = () => {
-        this.tick();
-        //The returned intervalID is a numeric, non-zero value which identifies the timer created by the call to setInterval()
-        this.intervalId = setInterval(this.tick, 1000);
-    }
-
-    pause = () => {
-        clearInterval(this.intervalId);
-    }
-
-    tick = () => {
-        this.timeRemaining = this.timeRemaining - 1;
-    }
-
-    get timeRemaining(){
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
-}
-
 const durationInput = document.querySelector('#durationTime');
 const playButton = document.querySelector('#playButton');
 const pauseButton = document.querySelector('#pauseButton');
 
-const timer = new Timer(durationInput, playButton, pauseButton);
+const timer = new Timer(durationInput, playButton, pauseButton, {
+    onStart() {
+    console.log('Timer Started');
+    },
+    onTick() {
+        console.log('Timer Started to tick');
+    },
+    onComplete() {
+        console.log('Timer Stopped');
+    }
+});
---
+++ timer.js
@@ -0,0 +1,53 @@
+class Timer{
+    constructor(durationInput, playButton, pauseButton, callbacks){
+        this.durationInput = durationInput;
+        this.playButton = playButton;
+        this.pauseButton = pauseButton;
+
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+
+        this.playButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    //the reason we are using a arrow functions is because we can use 'this' keyword to access the methods and properties of that particular instance of the object
+    start = () => {
+        this.tick();
+        if (this.onStart) {
+            this.onStart();   
+        }
+        //The returned intervalID is a numeric, non-zero value which identifies the timer created by the call to setInterval()
+        this.intervalId = setInterval(this.tick, 1000);
+    }
+
+    pause = () => {
+        clearInterval(this.intervalId);
+    }
+
+    tick = () => {
+        //condition to check the underflow of the timer
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;
+            if (this.onTick) {
+                this.onTick(); 
+            }
+        }    
+    }
+
+    get timeRemaining(){
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
+}
