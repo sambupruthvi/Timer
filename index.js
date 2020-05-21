a8af9beb4ddb7554e1e23c47d842f3b4b48379ea
Pruthvi Sambu <psambu@mg.local>
2020-05-21 15:12:00 -0500

    basic timer function

---
+++ index.js
@@ -0,0 +1,32 @@
+class Timer{
+    constructor(durationInput, playButton, pauseButton){
+        this.durationInput = durationInput;
+        this.playButton = playButton;
+        this.pauseButton = pauseButton;
+
+        this.playButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    //the reason we are using a arrow functions is because we can use 'this' keyword to access the methods and properties of that particular instance of the object
+    start = () => {
+        this.tick();
+        //The returned intervalID is a numeric, non-zero value which identifies the timer created by the call to setInterval()
+        this.intervalId = setInterval(this.tick, 1000);
+    }
+
+    pause = () => {
+        clearInterval(this.intervalId);
+    }
+
+    tick = () => {
+        this.timerValue = parseFloat(this.durationInput.value) - 1;
+        this.durationInput.value = this.timerValue; 
+    }
+}
+
+const durationInput = document.querySelector('#durationTime');
+const playButton = document.querySelector('#playButton');
+const pauseButton = document.querySelector('#pauseButton');
+
+const timer = new Timer(durationInput, playButton, pauseButton);
